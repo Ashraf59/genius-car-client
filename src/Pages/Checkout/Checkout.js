cf6e3978155973613a2fd7d57c90fe1c250c110b
Ashraf59 <ashrafams59b@gmail.com>
2022-11-07 00:34:22 +0600

    deploy on vercel

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -26,7 +26,7 @@ const Checkout = () => {
 
         }
 
-        fetch('http://localhost:5000/orders', {
+        fetch('https://genius-car-server-two-kappa.vercel.app/orders', {
             method: 'POST',
             headers: {
                 'content-type' : 'application/json'
